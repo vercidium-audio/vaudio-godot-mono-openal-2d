8a99eb6abb0a6e8301678a4051c8734c775d043f
Mitch RTX <mitchell@vercidium.com>
2026-08-28 13:35:24 +1000

    WIP groundwork

---
+++ misc/TransformWatcher.cs
@@ -0,0 +1,19 @@
+namespace vaudio_godot_mono_openal;
+
+// Lightweight child node that fires OnTransformChanged when its parent's global transform changes.
+// SetNotifyTransform is called here (not on the parent) so only this node gets the notification.
+partial class TransformWatcher : Node2D
+{
+    public Action OnTransformChanged { get; set; }
+
+    public override void _Ready()
+    {
+        SetNotifyTransform(true);
+    }
+
+    public override void _Notification(int what)
+    {
+        if (what == NotificationTransformChanged)
+            OnTransformChanged?.Invoke();
+    }
+}
