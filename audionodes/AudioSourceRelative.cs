5933fb483e890a227030cc7af52b5c4a03d59cd1
Mitch RTX <mitchell@vercidium.com>
2026-09-01 12:06:22 +1000

    WIP FMOD

--- audionodes/AudioSourceRelative.cs
+++ audionodes/AudioSourceRelative.cs
@@ -3,7 +3,7 @@ namespace vaudio_godot_mono_openal;
 // A sound source relative to the listener with zero offset, e.g. footsteps, ambience, music.
 // Always AL_SOURCE_RELATIVE with a pinned origin position.
 [Tool]
-public partial class ALSourceRelative : ALSource
+public partial class AudioSourceRelative : AudioSource
 {
     protected override void ConfigureSource(IAudioSourceHandle source)
     {
